ffa9c9b067b5d5eb10fad599f87161bbd5a987a0
TJ Ram <tjram500@gmail.com>
2021-03-18 21:16:42 -0700

    events stuff

--- front/pages/events.tsx
+++ front/pages/events.tsx
@@ -1,13 +1,97 @@
 import Page from '@components/page'
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import EventIcon from '@material-ui/icons/Event';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+
+/*
+* useStyles function and Event component adapted from Folder List, Align List Items: https://material-ui.com/components/lists/
+*/
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+      display: 'inline',
+    }
+  }),
+);
 
 const Events = () => {
-  const { user, error, isLoading } = useUser();  
-
+  const classes = useStyles();
+  const { user, error, isLoading } = useUser();
+  const fakeEvents = [
+    {
+      title: "Science Class",
+      dateTime: Date.now().toLocaleString(),
+      host: "Mr. Smith",
+    },
+    {
+      title: "History Class",
+      dateTime: Date.now().toLocaleString(),
+      host: "Mr. Johnson",
+    },
+    {
+      title: "Math Class",
+      dateTime: Date.now().toLocaleString(),
+      host: "Mrs. Jackson",
+    },
+  ];
+  let events = [];
+  for (let i = 0; i < fakeEvents.length; i++) {
+    events.push(
+      <Event classes={classes} title={fakeEvents[i].title} dateTime={fakeEvents[i].dateTime} host={fakeEvents[i].host} />
+    );
+    events.push(
+      <Divider />
+    );
+  } 
   return (
     <Page header={false} activePage={"My Events"} title={user ? `Welcome, ${user.name}!` : "Welcome!"}>
-      
+      <List className={classes.root}>
+        {events}
+      </List>
     </Page>
+  )
+}
+
+const Event = ({classes, title, dateTime, host}) => {
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>
+          <EventIcon />
+        </Avatar>
+        </ListItemAvatar>
+        <ListItemText 
+          primary={title}
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={classes.inline}
+                color="textPrimary" 
+              >
+                Hosted by {host}
+              </Typography>
+                {" on " + dateTime} 
+            </React.Fragment>
+          }
+          />
+      </ListItem>
+
   )
 }
 

d3d59465d961df19fe38d955eeeac599624f04c8
Max Leiter <maxwell.leiter@gmail.com>
2021-03-21 18:48:25 -0700

    many changes

--- front/pages/events.tsx
+++ front/pages/events.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Page from '@components/page'
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import React from 'react';
@@ -8,6 +9,7 @@ import { withUserProp } from '../lib/withUserProp';
 import { CustomUserContext } from '../types';
 import EventPage from '@components/events';
 import Event from '@components/event';
+import makeServerCall from '@lib/makeServerCall';
 
 /*
 * useStyles function and Event component adapted from Folder List, Align List Items: https://material-ui.com/components/lists/
@@ -15,54 +17,34 @@ import Event from '@components/event';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      width: '100%',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-    },
     inline: {
       display: 'inline',
     }
   }),
 );
 
-const Events = ({ userContext }: { userContext: CustomUserContext}) => {
-  const { user, error, isLoading } = userContext;  
+const EventsPage = ({ userContext }: { userContext: CustomUserContext}) => {
+  const { user, error, isLoading } = userContext;
+  const [events, setEvents] = useState([]);
+  console.log(userContext)
+  if (isLoading) {
+    return <div></div>;
+  }
+
+  useEffect(() => {
+    if (user)
+      makeServerCall({ apiCall: `events/getEventsForUser/${user.id}`, method: "GET" }).then((data) => {
+        setEvents(data)
+      });
+  }, [user])
+  console.log(events)
   const classes = useStyles();
-  const fakeEvents = [
-    {
-      title: "Science Class",
-      dateTime: Date.now().toLocaleString(),
-      host: "Mr. Smith",
-    },
-    {
-      title: "History Class",
-      dateTime: Date.now().toLocaleString(),
-      host: "Mr. Johnson",
-    },
-    {
-      title: "Math Class",
-      dateTime: Date.now().toLocaleString(),
-      host: "Mrs. Jackson",
-    },
-  ];
-  let events = [];
-  for (let i = 0; i < fakeEvents.length; i++) {
-    events.push(
-      <Event title={fakeEvents[i].title} dateTime={fakeEvents[i].dateTime} host={fakeEvents[i].host} />
-    );
-    events.push(
-      <Divider />
-    );
-  } 
+
   return (
     <Page header={false} activePage={"My Events"} title={user ? `Welcome, ${user.name}!` : "Welcome!"} userContext={userContext}>
-        <EventPage user={userContext.user} />
-        <List className={classes.root}>
-        {events}
-      </List>
+        <EventPage user={userContext.user} events={events} />
     </Page>
   )
 }
 
-export default withPageAuthRequired(withUserProp(Events))
+export default withPageAuthRequired(withUserProp(EventsPage))

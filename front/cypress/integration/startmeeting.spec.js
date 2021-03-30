2c270dcf916e99460fda4b6228dfb478448bf71f
Elissa Perdue <elissaperdue@gmail.com>
2021-03-30 15:15:16 -0700

    startmeeting

--- front/cypress/integration/startmeeting.spec.js
+++ front/cypress/integration/startmeeting.spec.js
@@ -1,5 +1,13 @@
 /// <reference types="cypress" />
-
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 context('Actions', () => {
     beforeEach(() => {
     //   cy.clearLocalStorage()
@@ -9,11 +17,24 @@ context('Actions', () => {
     it('.vist() - visit home', () => {
       cy.visit('http://localhost:3000')
 
-      cy.get('#loginButton').last().click()
+      // cy.get('#loginButton').last().click()
 
-      cy.get('#username').click().type('jhaeju20@gmail.com')
+      cy.get('#loginButton').last().click()
+      cy.get('#username').click().type('testuser57588@gmail.com')
       cy.get('#password').click().type('testuser123!')
       cy.get('button').first().click()
+      cy.get('a[href*="events"]').first().click()
+      cy.get('#newEvent').first().click()
+      let random_string1 = "Meeting_" + makeid(5);
+      cy.get('#title').click().type(random_string1)
+      cy.get('#description').click().type('We will see this desription displayed')
+      cy.get('#picture').click().type('https://www.legacy.com/wp-content/uploads/2020/01/Sympathy-flowers-orange-1000-shutterstock_694680475.jpg')
+    //   cy.get('#rdt').select('')
+      cy.get('[class="form-control"]').click()
+      cy.get('[class="rdtPicker"]').click('center')
+      cy.get('body').click(0,0);
+      cy.get('#save').click()
+      cy.location('pathname').should('include', 'event')
       cy.get('a[href*="events"]').first().click()
       cy.get('a[href*="event/"]').first().click()
       cy.wait(2000)
@@ -21,23 +42,6 @@ context('Actions', () => {
       cy.get('#start').first().click()
       cy.wait(5000)
       cy.get('#logoutButton').click()
-    //   cy.get('#newEvent').first().click()
-    //   cy.get('#title').click().type('MyCoolEventForTestingPurposes')
-    //   cy.get('#description').click().type('We will see this desription displayed')
-    //   cy.get('#picture').click().type('https://www.legacy.com/wp-content/uploads/2020/01/Sympathy-flowers-orange-1000-shutterstock_694680475.jpg')
-    // //   cy.get('#rdt').select('')
-    //   cy.get('[class="form-control"]').click()
-    //   cy.get('[class="rdtPicker"]').click('center')
-    //   cy.get('body').click(0,0);
-    //   cy.get('#save').click()
-    //   cy.wait(5000)
-    //   cy.get('#logoutButton').click()
-
-    //   cy.get('form-control').click()
-      // const auth0State = {
-      //   nonce: "", cy.get('span#username')
-      //   state: "some-random-state"
-      // }
     })
     
   

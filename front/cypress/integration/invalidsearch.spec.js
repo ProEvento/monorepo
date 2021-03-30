25ae85473f5fd8205eefb3093ab9e7ce280c09bb
Elissa Perdue <elissaperdue@gmail.com>
2021-03-30 13:14:39 -0700

    cleaning up tests

--- front/cypress/integration/invalidsearch.spec.js
+++ front/cypress/integration/invalidsearch.spec.js
@@ -58,60 +58,8 @@ function makeRandomInt(max) {
         cy.get('body').click(0,0);
         cy.get('#search').click()
         cy.wait(1000)
-        // cy.get('#logoutButton').click()
+        cy.get('#logoutButton').click()
     
-      })
-  
-    // it('.searchbydate() - visit home', () => {
-    //     cy.visit('http://localhost:3000')
-
-    //     cy.get('#loginButton').last().click()
-
-    //     // cy.get('#username').click().type('jhaeju20@gmail.com')
-    //     // cy.get('#password').click().type('testuser123!')
-    //     // cy.get('button').first().click()
-    //     cy.get('a[href*="explore"]').first().click()
-    //     cy.get('#simple-tab-2').click(); 
-    //     cy.get('[class="react-datepicker-wrapper"]').first().click()
-    //     cy.wait(1000)
-    //     cy.contains('7').click();
-    //     cy.wait(1000)
-
-    //     cy.get('body').click(0,0);
-    //     cy.get('[class="react-datepicker-wrapper"]').last().click()
-    //     cy.wait(1000)
-
-    //     cy.get('[class="react-datepicker__day react-datepicker__day--011"]').click()
-    //     cy.wait(1000)
-
-    //     cy.get('body').click(0,0);
-    //     cy.get('#search').click()
-
-    //     cy.wait(2000)
-
-    //     cy.get('a[href*="explore"]').first().click()
-    //     cy.get('#simple-tab-2').click(); 
-    //     cy.get('[class="react-datepicker-wrapper"]').first().click()
-    //     cy.wait(1000)
-    //     cy.contains('20').click();
-    //     cy.wait(1000)
-
-    //     cy.get('body').click(0,0);
-    //     cy.get('[class="react-datepicker-wrapper"]').last().click()
-    //     cy.wait(1000)
-
-    //     cy.contains('28').click();
-    //     cy.wait(1000)
-
-    //     cy.get('body').click(0,0);
-    //     cy.get('#search').click()
-    //     cy.wait(1000)
-
-
-    //     // cy.get('#logoutButton').click()
-    
-    // })
-      
-  
+      })  
     })
     

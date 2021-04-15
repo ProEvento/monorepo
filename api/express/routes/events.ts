09dc5cbb081238fcb5e0a547e3b402cc08b91171
TJ Ram <tjram500@gmail.com>
2021-04-14 22:32:23 -0700

    all

--- api/express/routes/events.ts
+++ api/express/routes/events.ts
@@ -69,6 +69,11 @@ async function startEvent(req: Request, res: Response) {
 	const event = await models.Event.findByPk(id);
 	//@ts-ignore
 	//console.log(id, event.title)
+	//@ts-ignore
+	const attendees = await event.getAttendees();
+	for (const user of attendees) {
+		await user.createNotification({text: "Your event " + event.title + " is starting now! Join here: /event/" + event.id})
+	}
 	if (event) {
 		//@ts-ignore
 		const dateEvent = new Date(event.time)
